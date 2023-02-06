e3b52201edd2327b9a68ec5a457ecd59d30c77b8
Pawel Owiesek <owiesek.p@gmail.com>
2023-02-06 14:39:41 +0100

    changed all needed let to const

--- js/script.js
+++ js/script.js
@@ -1,87 +1,89 @@
-let changeBackgroungImage = () => {
-  let body = document.querySelector(".js-body");
-  let imageButton = document.querySelector(".js-imageButton");
-  imageButton.addEventListener("click", () => {
-    body.classList.toggle("sun");
+{
+  const changeBackgroungImage = () => {
+    const body = document.querySelector(".js-body");
+    const imageButton = document.querySelector(".js-imageButton");
+    imageButton.addEventListener("click", () => {
+      body.classList.toggle("sun");
+    });
+  };
+  
+  changeBackgroungImage();
+  
+  const form = document.querySelector(".js-form");
+  const amountElement = document.querySelector(".js-amount");
+  const firstValue = document.querySelector(".js-firstValue");
+  const secondValue = document.querySelector(".js-secondValue");
+  let outcome;
+  const result = document.querySelector(".js-result");
+  const exchangeRate=document.querySelector(".js-exchangeRate");
+  
+  
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const currency1 = firstValue.value;
+    const amount = +amountElement.value;
+    const currency2 = secondValue.value;
+    const Eur = 4.692;
+    const Usd = 4.2928;
+    const Gbp = 5.2571;
+    const Pln = 1;
+    const currencyChange = `${currency1}/${currency2}`;
+  
+    switch (currencyChange) {
+      case "Eur/Eur":
+        outcome = Eur / Eur;
+        break;
+      case "Eur/Usd":
+        outcome = Eur / Usd;
+        break;
+      case "Eur/Gbp":
+        outcome = Eur / Gbp;
+        break;
+      case "Eur/Pln":
+        outcome = Eur / Pln;
+        break;
+      case "Usd/Usd":
+        outcome = Usd / Usd;
+        break;
+      case "Usd/Eur":
+        outcome = Usd / Eur;
+        break;
+      case "Usd/Gbp":
+        outcome = Usd / Gbp;
+        break;
+      case "Usd/Pln":
+        outcome = Usd / Pln;
+        break;
+      case "Pln/Pln":
+        outcome = Pln / Pln;
+        break;
+      case "Pln/Eur":
+        outcome = Pln / Eur;
+        break;
+      case "Pln/Usd":
+        outcome = Pln / Usd;
+        break;
+      case "Pln/Gbp":
+        outcome = Pln / Gbp;
+        break;
+      case "Gbp/Gbp":
+        outcome = Gbp / Gbp;
+        break;
+      case "GbpPln/Eur":
+        outcome = Gbp/ Eur;
+        break;
+      case "GbpPln/Usd":
+        outcome = Gbp / Usd;
+        break;
+      case "GbpPln/Pln":
+        outcome = Gbp / Pln;
+        break;
+    }
+  
+    const resultat = outcome * amount;
+    result.innerText = `${resultat.toFixed(2)}`;
+    exchangeRate.innerText=`${outcome.toFixed(2)}`;
   });
-};
-
-changeBackgroungImage();
-
-let form = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let firstValue = document.querySelector(".js-firstValue");
-let secondValue = document.querySelector(".js-secondValue");
-let outcome;
-let result = document.querySelector(".js-result");
-let exchangeRate=document.querySelector(".js-exchangeRate");
-
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let currency1 = firstValue.value;
-  let amount = +amountElement.value;
-  let currency2 = secondValue.value;
-  let Eur = 4.692;
-  let Usd = 4.2928;
-  let Gbp = 5.2571;
-  let Pln = 1;
-  let currencyChange = `${currency1}/${currency2}`;
-
-  switch (currencyChange) {
-    case "Eur/Eur":
-      outcome = Eur / Eur;
-      break;
-    case "Eur/Usd":
-      outcome = Eur / Usd;
-      break;
-    case "Eur/Gbp":
-      outcome = Eur / Gbp;
-      break;
-    case "Eur/Pln":
-      outcome = Eur / Pln;
-      break;
-    case "Usd/Usd":
-      outcome = Usd / Usd;
-      break;
-    case "Usd/Eur":
-      outcome = Usd / Eur;
-      break;
-    case "Usd/Gbp":
-      outcome = Usd / Gbp;
-      break;
-    case "Usd/Pln":
-      outcome = Usd / Pln;
-      break;
-    case "Pln/Pln":
-      outcome = Pln / Pln;
-      break;
-    case "Pln/Eur":
-      outcome = Pln / Eur;
-      break;
-    case "Pln/Usd":
-      outcome = Pln / Usd;
-      break;
-    case "Pln/Gbp":
-      outcome = Pln / Gbp;
-      break;
-    case "Gbp/Gbp":
-      outcome = Gbp / Gbp;
-      break;
-    case "GbpPln/Eur":
-      outcome = Gbp/ Eur;
-      break;
-    case "GbpPln/Usd":
-      outcome = Gbp / Usd;
-      break;
-    case "GbpPln/Pln":
-      outcome = Gbp / Pln;
-      break;
-  }
-
-  let resultat = outcome * amount;
-  result.innerText = `${resultat.toFixed(2)}`;
-  exchangeRate.innerText=`${outcome.toFixed(2)}`;
-});
-
+  
+}
 
